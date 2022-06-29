16010bf264798488a3cb2414500bb03aba56b53d
Jiancheng LI <jiancheng.li@thoughtworks.com>
2022-06-29 12:15:53 +0800

    add formula

---
+++ testscript.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/JCLTW/testscript/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "b69d4711280b710d4d27071aa0d75f68beb509f965fb6906633bf804c6932110"
+  license ""
+
+  def install
+    bin.install "testscript"
+  end
+
+end
